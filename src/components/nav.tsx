281561795c4bf31f134c036a58bfcf2dfb181eb7
iamyunusali <aliyunus291@gmail.com>
2023-03-09 07:19:56 +0300

    Add navigations

---
+++ src/components/nav.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className=" flex flex-row font-bold justify-between items-center px-[4%]">
+        <div>
+            <h1>Maed Crypt</h1>
+        </div>
+        <div>
+           <ul className="flex gap-[20px] list-none">
+            <li>
+                Home
+            </li>
+            <li>
+                MarketPlace
+            </li>
+            <li>
+                Coin list
+            </li>
+            <li>
+                News
+            </li>
+            </ul>
+        </div>
+        <div className="flex gap-[5px]">
+            <button type="button">Login</button>
+            <button type="button"> Sign up</button>
+        </div>
+        </div>
+    )
+}
+
+export default Nav;

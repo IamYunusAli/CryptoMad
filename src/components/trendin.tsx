cbf5820e9aa451c68367b0fdbf9850af0eff8a17
iamyunusali <aliyunus291@gmail.com>
2023-03-11 17:36:13 +0300

    Add trending section

---
+++ src/components/trendin.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Trend = () => {
+    return (
+        <>
+        <h1 className="font-bold font-sans md:text-5xl text-3xl"> Trending news </h1>
+        <div className="grid grid-cols-4 gap-5 mx-5 my-10" >
+        <div className="h-[450px] bg-[#869BDF] opacity-20 rounded-md">
+
+        </div>
+        <div className="h-[450px] bg-[#869BDF] opacity-20 rounded-md">
+
+        </div>
+        <div className="h-[450px] bg-[#869BDF] opacity-20 rounded-md">
+
+        </div>
+        <div className="h-[450px] bg-[#869BDF] opacity-20 rounded-md">
+
+        </div>
+        </div>
+        </>
+    )
+}
+
+export default Trend;
